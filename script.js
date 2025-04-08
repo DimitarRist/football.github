fc36f41c6591a6277e60e9e09a4c7be55f67e8c2
DimitarRist <dimitarristovski0@gmail.com>
2025-04-08 20:18:09 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
+function updateclock(){
+    const clock =
+document.getElementByld("live-clock");
+    const now = new Date();
+    clock.innerText = now.toLocaleTimeString();
+}
 
+setInterval(updateClock, 1000);
+
+updateClock();
